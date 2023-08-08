6a44622b29e023f87ab519be4514b9a6f39d188d
Kevin <kevinroberto026@gmail.com>
2023-08-08 17:22:00 -0600

    initial commit

---
+++ Ejemplo2/js/rectangulo.js
@@ -0,0 +1,5 @@
+var base = prompt('Introduzca la base del rectángulo:','');
+var altura = prompt('Introduzca la altura del rectángulo:','');
+var area;
+area = base * altura;
+document.write("<header><h1>El área del rectangulo es: " + area + "</h1><hr /><br /></header>");
---
+++ Ejemplo2/js/triangulo.js
@@ -0,0 +1,5 @@
+var base = prompt('Introduzca la base del triángulo:','');
+var altura = prompt('Introduzca la altura del triángulo:','');
+var area;
+area = (base * altura)/2;
+document.write("<header><h1>El área del triángulo es: " + area + "</h1><hr /><br /></header>");
